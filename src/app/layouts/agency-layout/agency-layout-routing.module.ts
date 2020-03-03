23b3d05af83ebd95b84d3c9f8318ae7a10c5f51c
Jitesh Yadav <jitesh.yadav@nagarro.com>
2020-03-03 07:49:21 +0530

    form ui initial changes

--- src/app/layouts/agency-layout/agency-layout-routing.module.ts
+++ src/app/layouts/agency-layout/agency-layout-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AgencyLayoutComponent } from './agency-layout.component';
 import { AgencyDashboardComponent } from './pages/agency-dashboard/agency-dashboard.component';
 import { AgencyClientsComponent } from './pages/agency-clients/agency-clients.component';
-import { AgencyPoliciesComponent } from './pages/agency-policies/agency-policies.component';
 import { AddClientComponent } from './pages/add-client/add-client.component';
+import { ClientEmployeesComponent } from './pages/client-employees/client-employees.component';
 
 const routes: Routes = [
   {
@@ -16,8 +15,8 @@ const routes: Routes = [
     component: AgencyClientsComponent
   },
   {
-    path: "policies",
-    component: AgencyPoliciesComponent
+    path: "employees",
+    component: ClientEmployeesComponent
   },
   {
     path: "client/add",
--- src/app/layouts/agency-layout/constants/agency.constants.ts
+++ src/app/layouts/agency-layout/constants/agency.constants.ts
@@ -2,7 +2,7 @@ import { IRouteInfo } from '../../shared/interfaces/route-info.interface';
 
 export const AGENCY_ROUTES: IRouteInfo[] = [
   { path: '/agency/dashboard',   title: 'Dashboard',    icon:'fa-cube'},
-  { path: '/agency/clients',     title: 'Clients',      icon:'fa-users'},
-  { path: '/agency/policies',    title: 'Policies',     icon:'fa-sticky-note'},
-  { path: '/agency/client/add',  title: 'Add a Client', icon:'fa-user-plus'},
+  { path: '/agency/clients',     title: 'Clients',      icon:'fa-address-book'},
+  { path: '/agency/employees',    title: 'Employees',     icon:'fa-users'},
+  { path: '/agency/client/add',  title: 'Add a Client', icon:'fa-plus'},
 ];
